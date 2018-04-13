aaeb13a549425f5bc95ed3083fb8d1951800f6ba
antkahn <ant.kahn@gmail.com>
2018-04-13 17:23:24 +0200

    change drawer closing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from './Header';
-import MuiThemeProvider from '../node_modules/material-ui/styles/MuiThemeProvider';
-import RaisedButton from '../node_modules/material-ui/RaisedButton';
-import FontIcon from '../node_modules/material-ui/FontIcon';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./Header";
+import MuiThemeProvider from "../node_modules/material-ui/styles/MuiThemeProvider";
+import RaisedButton from "../node_modules/material-ui/RaisedButton";
+import FontIcon from "../node_modules/material-ui/FontIcon";
 
 class App extends Component {
-
   render() {
     return (
-      <div className="App">
-        <MuiThemeProvider>
-          <Header/>
-        </MuiThemeProvider>
-        <MuiThemeProvider>
+      <MuiThemeProvider>
+        <div className="App">
+          <Header />
           <RaisedButton
             label="Nouveau Match"
             icon={<FontIcon className="muidocs-icon-people" />}
           />
-        </MuiThemeProvider>
-      </div>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
-
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,40 +1,44 @@
-import React, { Component } from 'react';
-import AppBar from '../node_modules/material-ui/AppBar';
-import Drawer from '../node_modules/material-ui/Drawer';
-import MenuItem from '../node_modules/material-ui/MenuItem';
+import React, { Component, Fragment } from "react";
+import AppBar from "../node_modules/material-ui/AppBar";
+import Drawer from "../node_modules/material-ui/Drawer";
+import MenuItem from "../node_modules/material-ui/MenuItem";
 
 class Header extends Component {
-    state = {
-        open: false,
-    }
+  state = {
+    open: false
+  };
 
-    displayMenu () {
-        this.setState({open: !this.state.open});
-    }
+  displayMenu() {
+    this.setState({ open: !this.state.open });
+  }
 
-    handleClose () {
-        this.setState({open: false});
-    }
+  handleClose() {
+    this.setState({ open: false });
+  }
 
-    render() {
-        return (
-            <div>
-                <Drawer 
-                open={this.state.open}
-                onRequestChange={(open) => this.setState({open})}>
-                    <MenuItem onClick={() => this.handleClose()}>Accueil</MenuItem>
-                    <MenuItem onClick={() => this.handleClose()}>Les joueurs</MenuItem>
-                    <MenuItem onClick={() => this.handleClose()}>Résultats</MenuItem>
-                    <MenuItem onClick={() => this.handleClose()}>Classements</MenuItem>
-                    <MenuItem onClick={() => this.handleClose()}>Trophées individuels</MenuItem>
-                </Drawer>
-                <AppBar
-                    title="Résultats FiFa Chambre 4B309" 
-                    onLeftIconButtonClick={() => this.displayMenu()}
-                />
-            </div>
-        );
-    }
-} 
+  render() {
+    return (
+      <Fragment>
+        <Drawer
+          docked={false}
+          open={this.state.open}
+          onRequestChange={open => this.setState({ open })}
+        >
+          <MenuItem onClick={() => this.handleClose()}>Accueil</MenuItem>
+          <MenuItem onClick={() => this.handleClose()}>Les joueurs</MenuItem>
+          <MenuItem onClick={() => this.handleClose()}>Résultats</MenuItem>
+          <MenuItem onClick={() => this.handleClose()}>Classements</MenuItem>
+          <MenuItem onClick={() => this.handleClose()}>
+            Trophées individuels
+          </MenuItem>
+        </Drawer>
+        <AppBar
+          title="Résultats FiFa Chambre 4B309"
+          onLeftIconButtonClick={() => this.displayMenu()}
+        />
+      </Fragment>
+    );
+  }
+}
 
 export default Header;
