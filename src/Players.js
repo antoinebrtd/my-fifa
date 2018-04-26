4cb9c27620086ab12056a5b4d3197603088cc215
Beber92 <antoine.bertrand@student.ecp.fr>
2018-04-26 17:40:58 +0200

    add cards and buttons

--- src/Players.js
+++ src/Players.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Tabs, Tab } from "material-ui";
+import { Tabs, Tab, Card, CardMedia, CardTitle, CardText, CardActions, FlatButton } from "material-ui";
 import SwipeableViews from 'react-swipeable-views';
 
 class Players extends Component {
@@ -13,23 +13,23 @@ class Players extends Component {
 
   render() {
     const styles = {
-      headline: {
-        fontSize: 24,
-        paddingTop: 16,
-        marginBottom: 12,
-        fontWeight: 400,
+      tabs: {
+        width: "90vw",
+        margin: "auto"
       },
       slide: {
-        padding: 10,
-      },
+        width: "90vw",
+        height: "80vh",
+        margin: "auto"
+      }
     };  
 
     return (
       <div>
-        <h1>Les Joueurs</h1>
         <Tabs
           onChange={this.handleChange}
           value={this.state.slideIndex}
+          style={styles.tabs}
         >
           <Tab label="Borcky" value={0} />
           <Tab label="Tutur" value={1} />
@@ -38,32 +38,57 @@ class Players extends Component {
         <SwipeableViews
           index={this.state.slideIndex}
           onChangeIndex={this.handleChange}
+          style={styles.slide}
         >
           <div>
-            <h2 style={styles.headline}>Le Diplodocus</h2>
-            <p>Ce grand dinosaure sorti tout droit du jurassique saura jouer de sa taille et de son long cou
-               pour faire planer une menace constante sur les défenses. Sa chevelure blonde toujours coiffée 
-               impeccablement lui confère une adhérence toute partciulière sur le cuir: tête piquée, plongeante ou en lucarne, 
-               il placera le ballon où il le souhaite sur le corner gagnant à la 93ème minute en finale de Ligue Des Champions. 
-               Avec lui, pas de doutes: le danger est aérien.
-            </p>
+            <Card>
+              <CardMedia>
+              </CardMedia>
+              <CardTitle title="Le diplodocus" />
+              <CardText>
+                Ce grand dinosaure sorti tout droit du jurassique saura jouer de sa taille et de son long cou
+                pour faire planer une menace constante sur les défenses. Sa chevelure blonde toujours coiffée 
+                impeccablement lui confère une adhérence toute partciulière sur le cuir: tête piquée, plongeante ou en lucarne, 
+                il placera le ballon où il le souhaite sur le corner gagnant à la 93ème minute en finale de Ligue Des Champions. 
+                Avec lui, pas de doutes: le danger est aérien.
+              </CardText>
+              <CardActions>
+                <FlatButton label="Voir l'armoire à trophées" />
+              </CardActions>
+            </Card>
           </div>
           <div>
-            <h2 style={styles.headline}>La Terreur du Vésinet</h2>
-            <p>Cet enfant turbulent du 78110 mettra à profit son arrogance pour vous déstabiliser dès le début du match. 
-               Lancé à pleine vitesse à 11km/h, il saura user de ses épaules d'une envergure de 3m50 pour se frayer
-               un chemin de force entre les défenseurs centraux. Même Ramos et Pepe ne sortiraient pas indemnes d'un tel combat.
-               Un conseil, ne lui racontez pas votre life, sinon gare aux coups de coude sous le menton,
-               il n'hésitera pas à vous casser le front.
-            </p>
+          <Card>
+              <CardMedia>
+              </CardMedia>
+              <CardTitle title="La Terreur du Vésinet" />
+              <CardText>
+                Cet enfant turbulent du 78110 mettra à profit son arrogance pour vous déstabiliser dès le début du match. 
+                Lancé à pleine vitesse à 11km/h, il saura user de ses épaules d'une envergure de 3m50 pour se frayer
+                un chemin de force entre les défenseurs centraux. Même Ramos et Pepe ne sortiraient pas indemnes d'un tel combat.
+                Un conseil, ne lui racontez pas votre life, sinon gare aux coups de coude sous le menton,
+                il n'hésitera pas à vous casser le front.
+              </CardText>
+              <CardActions>
+                <FlatButton label="Voir l'armoire à trophées" />
+              </CardActions>
+            </Card>
           </div>
           <div>
-            <h2 style={styles.headline}>Jean Beauf</h2>
-            <p>Ce tonton blagueur venu tout droit du camping en claquettes chaussettes vous distraira de son humour 
-               bien trempé, pour mieux vous porter l'estocade. Grâce à son sommeil réparateur, il sera toujours prêt pour les 
-               grandes échéances, mais sa susceptibilité lui joue souvent des tours, à tel point qu'il a décidé de raccrocher les crampons
-               il y a 5 mois après une légendaire humiliation 0-4. Il prépare dans l'ombre son grand retour, alors attention aux lancers de manettes.
-            </p>
+          <Card>
+              <CardMedia>
+              </CardMedia>
+              <CardTitle title="Jean Beauf" />
+              <CardText>
+                Ce tonton blagueur venu tout droit du camping en claquettes chaussettes vous distraira de son humour 
+                bien trempé, pour mieux vous porter l'estocade. Grâce à son sommeil réparateur, il sera toujours prêt pour les 
+                grandes échéances, mais sa susceptibilité lui joue souvent des tours, à tel point qu'il a décidé de raccrocher les crampons
+                après une légendaire humiliation 0-4. Il prépare son grand retour, alors attention aux lancers de manettes.
+              </CardText>
+              <CardActions>
+                <FlatButton label="Voir l'armoire à trophées" />
+              </CardActions>
+            </Card>
           </div>
         </SwipeableViews>
       </div>
