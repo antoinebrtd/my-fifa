509f4a591aa65d08072673649684b235839f20f6
beber92 <antoine.bertrand@student.ecp.fr>
2018-07-04 11:36:31 +0200

    update reducers

--- src/reducers.js
+++ src/reducers.js
@@ -4,13 +4,17 @@ import playersReducer from "./redux/ui/players/Players.reducer";
 import addMatchReducer from "./redux/entities/addMatch/AddMatch.reducer";
 import fetchMatchReducer from "./redux/entities/fetchMatch/FetchMatch.reducer";
 import bottomNavReducer from "./redux/ui/bottomNav/BottomNav.reducer";
+import fetchPlayersReducer from "./redux/entities/fetchPlayers/FetchPlayers.reducer";
+import fetchTeamsReducer from "./redux/entities/fetchTeams/FetchTeams.reducer";
 
 const rootReducer = combineReducers({
   header: headerReducer,
   players: playersReducer,
   addMatch: addMatchReducer,
   fetchMatch: fetchMatchReducer,
-  bottomNav: bottomNavReducer
+  bottomNav: bottomNavReducer,
+  fetchPlayers: fetchPlayersReducer,
+  fetchTeams: fetchTeamsReducer
 });
 
 export default rootReducer;
